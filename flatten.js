516afa6c9092a8bf6d545d23a4fd9b34fc631a29
Huwaida1990 <huwaida_khaild@yahoo.com>
2022-01-31 11:06:31 -0700

    flatten

---
+++ flatten.js
@@ -0,0 +1,10 @@
+const flatten = function(array){
+  // for(item of array){
+  //   if(typeof item === 'object') item.
+
+  // }
+  // return console.log(array)
+  return console.log(array.flat())
+}
+
+flatten([1,2,4,[4,5,7],9])
