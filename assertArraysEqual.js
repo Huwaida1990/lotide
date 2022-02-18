f3ae7f39d456a9688cee48f0dfe351aa6bd25047
Huwaida1990 <huwaida_khaild@yahoo.com>
2022-02-17 22:11:07 -0700

    k

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,13 +1,12 @@
-const assertEqual = function(actual, expected) {
-  if(actual.toString() === expected.toString()) {
-    console.log(' Assertion Passed: '+ actual + ' === '  + expected)
-    return true
-  }else{
-    console.log(' Assertion Faild: '+ actual + ' !== '  + expected)
+assertEqual(['1',2],['1',2])
+const eqArrays = require("./eqArrays");
 
-    return false
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅  Assertion passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑  Assertion failed: ${array1} !== ${array2}`);
   }
-
 };
 
-assertEqual(['1',2],['1',2])
+module.exports = assertArraysEqual;
